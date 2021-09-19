5a1c6f7a4a23ac1e85f66ffeb5e478ad54a1d247
Yulia <jbachareva@gmail.com>
2021-09-19 15:40:08 +0600

    HW1

---
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Products
+{
+    public class ProductsClient : BaseClient, IProductData
+    {
+        public ProductsClient(IConfiguration Configuration) : base(Configuration, WebAPI.Products) { }
+
+        public IEnumerable<Section> GetSections() => Get<IEnumerable<Section>>($"{_ServiceAddress}/sections");
+
+        public Section GetSection(int Id) => Get<Section>($"{_ServiceAddress}/section/{Id}");
+
+        public IEnumerable<Brand> GetBrands() => Get<IEnumerable<Brand>>($"{_ServiceAddress}/brands");
+
+        public Brand GetBrand(int Id) => Get<Brand>($"{_ServiceAddress}/brand/{Id}");
+
+        public PageProductsDTO GetProducts(ProductFilter Filter = null) =>
+            Post(_ServiceAddress, Filter ?? new ProductFilter())
+               .Content
+               .ReadAsAsync<PageProductsDTO>()
+               .Result;
+
+        public ProductDTO GetProductById(int id) => Get<ProductDTO>($"{_ServiceAddress}/{id}");
+    }
+}
