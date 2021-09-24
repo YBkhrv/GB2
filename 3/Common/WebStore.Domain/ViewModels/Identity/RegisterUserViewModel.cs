3f802466a0129f672b6f7285916a77332a328f7e
Yulia <jbachareva@gmail.com>
2021-09-24 10:07:16 +0600

    HW3

---
+++ 3/Common/WebStore.Domain/ViewModels/Identity/RegisterUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Domain.ViewModels.Identity
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Минимальная длина 3 символа")]
+        [MaxLength(256)]
+        [Display(Name = "Имя пользователя")]
+        [Remote("IsNameFree", "Account")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ 3/Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Products)]
+    [ApiController]
+    public class ProductsApiController : ControllerBase, IProductData
+    {
+        private readonly IProductData _ProductData;
+
+        public ProductsApiController(IProductData ProductData) => _ProductData = ProductData;
+
+        [HttpGet("sections")] //api/products/sections
+        public IEnumerable<Section> GetSections() => _ProductData.GetSections();
+
+        [HttpGet("section/{Id}")]
+        public Section GetSection(int Id) => _ProductData.GetSection(Id);
+
+        [HttpGet("brands")] //api/products/brands
+        public IEnumerable<Brand> GetBrands() => _ProductData.GetBrands();
+
+        [HttpGet("brand/{Id}")]
+        public Brand GetBrand(int Id) => _ProductData.GetBrand(Id);
+
+        [HttpPost]
+        public PageProductsDTO GetProducts([FromBody] ProductFilter Filter = null) => _ProductData.GetProducts(Filter);
+
+        [HttpGet("{id}")]
+        public ProductDTO GetProductById(int id) => _ProductData.GetProductById(id);
+    }
+}
