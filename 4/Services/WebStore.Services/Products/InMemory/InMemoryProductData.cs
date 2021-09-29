045d473daf366c85aee1bd654e2a8042bb66ceac
Yulia <jbachareva@gmail.com>
2021-09-29 12:36:20 +0600

    HW4

---
+++ 4/Services/WebStore.Services/Products/InMemory/InMemoryProductData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Data;
+using WebStore.Services.Mapping;
+
+namespace WebStore.Services.Products.InMemory
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Section> GetSections() => TestData.Sections;
+
+        public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null)
+        {
+            var query = TestData.Products;
+
+            if (Filter?.SectionId != null)
+                query = query.Where(product => product.SectionId == Filter.SectionId);
+
+            if (Filter?.BrandId != null)
+                query = query.Where(product => product.BrandId == Filter.BrandId);
+
+            return query.Select(p => p.ToDTO());
+        }
+
+        public ProductDTO GetProductById(int id) => TestData.Products.FirstOrDefault(p => p.Id == id).ToDTO();
+    }
+}
---
+++ 4/Services/WebStore.Services/Products/InSQL/SqlProductData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Mapping;
+
+namespace WebStore.Services.Products.InSQL
+{
+    public class SqlProductData : IProductData
+    {
+        private readonly WebStoreDB _db;
+
+        public SqlProductData(WebStoreDB db) => _db = db;
+
+        public IEnumerable<Section> GetSections() => _db.Sections;
+
+        public IEnumerable<Brand> GetBrands() => _db.Brands;
+
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null)
+        {
+            IQueryable<Product> query = _db.Products;
+
+            if (Filter?.Ids?.Length > 0)
+                query = query.Where(product => Filter.Ids.Contains(product.Id));
+            else
+            {
+                if (Filter?.BrandId != null)
+                    query = query.Where(product => product.BrandId == Filter.BrandId);
+
+                if (Filter?.SectionId != null)
+                    query = query.Where(product => product.SectionId == Filter.SectionId);
+            }
+
+            return query.Select(p => p.ToDTO());
+        }
+
+        public ProductDTO GetProductById(int id) => _db.Products
+           .Include(p => p.Section)
+           .Include(p => p.Brand)
+           .FirstOrDefault(p => p.Id == id)
+           .ToDTO();
+    }
+}
