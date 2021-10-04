5cd7d90af245e6f2d2f13c191e668f36153511e5
Yulia <jbachareva@gmail.com>
2021-10-04 12:04:10 +0600

    HW5

---
+++ 5/HW5/UI/WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Mapping;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index() => View(_ProductData.GetProducts().Select(p => p.FromDTO()));
+
+        public IActionResult Edit(int? id)
+        {
+            var product = id is null ? new Product() : _ProductData.GetProductById((int) id).FromDTO();
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product) => RedirectToAction(nameof(Index));
+
+        public IActionResult Delete(int id)
+        {
+            var product = _ProductData.GetProductById(id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product.FromDTO());
+        }
+
+        [HttpPost, ValidateAntiForgeryToken, ActionName(nameof(Delete))]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction(nameof(Index));
+    }
+}
